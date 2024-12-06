9ca2a207f5a6dd1e4f6d10b5d845fdb4e85d9a06
alena1805 <145384204+alena1805@users.noreply.github.com>
2024-12-06 10:25:25 +0500

    Create includes.h.cpp

---
+++ includes.h.cpp
@@ -0,0 +1,18 @@
+#ifndef INCS
+#define INCS
+
+#include <ios>
+#include <iostream>
+#include <fstream>
+// Подключаем необходимые заголовочные файлы для работы с вводом/выводом
+#include <string>
+#include <initializer_list>
+#include <vector>
+#include <utility>
+
+#define _clstyping template<typename _T>
+#define strbuff std::vector<std::string>
+
+
+#endif
+
